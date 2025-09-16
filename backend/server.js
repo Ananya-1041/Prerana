4e832e9ea45e74567bceef685a38d9591cef943a
Ananya-1041 <ananya.edurkala@gmail.com>
2025-09-16 19:23:57 +0530

    third commit

--- backend/server.js
+++ backend/server.js
@@ -8,7 +8,7 @@ const bcrypt = require('bcrypt');
 const fs = require('fs'); 
 
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000;
 
 app.use(cors());
 app.use(bodyParser.json());
